c467d732ed6ff59c8b8dfcc279a7a561f56cadd6
FoxFurry <isacartur@gmail.com>
2023-03-20 03:15:18 +0200

    Added env parsing to gateway

--- gateway/cmd/root.go
+++ gateway/cmd/root.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/FoxFurry/scholarlabs/gateway/internal/config"
 	"github.com/FoxFurry/scholarlabs/gateway/internal/server"
+	"github.com/caarlos0/env/v7"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +18,13 @@ var rootCmd = &cobra.Command{
 	Use:   "gateway",
 	Short: "Run HTTP(s) server for gateway service",
 	Run: func(cmd *cobra.Command, args []string) {
-		gateway, err := server.New()
+		cfg := config.Config{}
+
+		if err := env.Parse(&cfg); err != nil {
+			log.Fatalf("failed to load config")
+		}
+
+		gateway, err := server.New(cfg)
 		if err != nil {
 			log.Fatalf("failed to create a gateway server: %v", err)
 		}
