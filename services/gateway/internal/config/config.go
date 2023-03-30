cb7052aaa02afe6f27e0626a7409afa8aef7abb9
FoxFurry <isacartur@gmail.com>
2023-03-31 02:00:06 +0300

    Changed GatewayHost to jsut Host

--- services/gateway/internal/config/config.go
+++ services/gateway/internal/config/config.go
@@ -3,8 +3,8 @@ package config
 type Config struct {
 	Environment string `env:"ENV"`
 
-	GatewayHost string `env:"GATEWAY_HOST"`
-	SigningKey  string `env:"SIGNING_KEY"`
+	Host       string `env:"HOST"`
+	SigningKey string `env:"SIGNING_KEY"`
 
 	MarketplaceServiceBaseURL string `env:"MARKETPLACE_BASE_URL"`
 	UserServiceBaseURL        string `env:"USER_BASE_URL"`
