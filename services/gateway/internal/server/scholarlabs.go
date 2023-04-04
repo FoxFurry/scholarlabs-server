f3508088b077e6d42f52e48adc798154a9199546
FoxFurry <isacartur@gmail.com>
2023-04-05 01:30:18 +0300

    Added endpoints for user service in gateway

--- services/gateway/internal/server/scholarlabs.go
+++ services/gateway/internal/server/scholarlabs.go
@@ -4,45 +4,49 @@ import (
 	"github.com/FoxFurry/scholarlabs/services/gateway/internal/config"
 	"github.com/FoxFurry/scholarlabs/services/gateway/internal/scholarlabs"
 	"github.com/FoxFurry/scholarlabs/services/gateway/internal/util"
+	"github.com/FoxFurry/scholarlabs/services/user/proto"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 type ScholarLabs struct {
-	service scholarlabs.Service
-	gEng    *gin.Engine
-	jwt     util.JWTProvider
-	cfg     config.Config
-	lg      *logrus.Logger
+	service     scholarlabs.Service
+	gEng        *gin.Engine
+	jwt         util.JWTProvider
+	cfg         config.Config
+	userService proto.UserClient
+	lg          *logrus.Logger
 }
 
-func New(cfg config.Config, logger *logrus.Logger) (*ScholarLabs, error) {
+func New(cfg config.Config, logger *logrus.Logger, userSrv proto.UserClient) (*ScholarLabs, error) {
 	gin.SetMode(gin.ReleaseMode)
 
 	ginEngine := gin.Default()
 
 	p := ScholarLabs{
-		service: scholarlabs.New(),
-		gEng:    ginEngine,
-		jwt:     util.NewJWT(),
-		cfg:     cfg,
-		lg:      logger,
+		service:     scholarlabs.New(),
+		gEng:        ginEngine,
+		jwt:         util.NewJWT(),
+		cfg:         cfg,
+		userService: userSrv,
+		lg:          logger,
 	}
 
 	v1 := ginEngine.Group("/v1")
 	v1.Use(p.corsMiddleware)
 	{
-
-		//plan := v1.Group("/plan")
-		//{
-		//	plan.POST("/", p.jwtMiddleware, p.CreatePlan)
-		//	plan.DELETE("/:planUUID", p.jwtMiddleware, p.DeletePlan)
-		//}
+		user := v1.Group("/user")
+		{
+			user.POST("/register", p.Register)
+			user.POST("/login", p.Login)
+		}
 	}
 
 	return &p, nil
 }
 
 func (p *ScholarLabs) Run() {
-	p.gEng.Run(p.cfg.Host)
+	if err := p.gEng.Run(p.cfg.Host); err != nil {
+		p.lg.Fatalf("failed to run http server: %v", err)
+	}
 }
