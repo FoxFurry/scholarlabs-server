b90dad644825649faaf6fb0687ded087d2587d97
FoxFurry <isacartur@gmail.com>
2023-04-05 01:31:50 +0300

    Added database credentials to user config

--- services/user/internal/config/config.go
+++ services/user/internal/config/config.go
@@ -1,20 +1,15 @@
 package config
 
 type Config struct {
-	Environment string `env:"ENV"`
+	Environment string `env:"ENV" envDefault:"development"`
 
-	DBHost string `env:"DB_HOST"`
-	DBPort string `env:"DB_PORT"`
-	DBPass string `env:"DB_PASS"`
-	DBUser string `env:"DB_USER"`
-	DBName string `env:"DB_NAME"`
+	DBHost string `env:"DB_HOST" envDefault:"localhost"`
+	DBPort string `env:"DB_PORT" envDefault:"3306"`
+	DBPass string `env:"DB_PASS" envDefault:"test"`
+	DBUser string `env:"DB_USER" envDefault:"user"`
+	DBName string `env:"DB_NAME" envDefault:"user"`
 
-	Host       string `env:"HOST"`
-	SigningKey string `env:"SIGNING_KEY"`
-
-	MarketplaceServiceBaseURL string `env:"MARKETPLACE_BASE_URL"`
-	UserServiceBaseURL        string `env:"USER_BASE_URL"`
-	CourseServiceBaseURL      string `env:"COURSE_BASE_URL"`
+	Host string `env:"HOST" envDefault:"0.0.0.0:22000"`
 
 	TokenSecret string `env:"TOKEN_SECRET"`
 }
