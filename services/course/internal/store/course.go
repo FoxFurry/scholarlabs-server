b6561fc5fa7ad23317012c75952596396ff64edc
FoxFurry <isacartur@gmail.com>
2023-04-23 16:28:39 +0300

    Implemented course data store

--- services/course/internal/store/course.go
+++ services/course/internal/store/course.go
@@ -5,38 +5,121 @@ import (
 	"time"
 )
 
-type User struct {
-	ID       uint64
-	UUID     string
-	Email    string
-	Username string
-	Password string
+type Course struct {
+	ID          uint64
+	UUID        string
+	AuthorUUID  string
+	Title       string
+	Description string
+	Thumbnail   string
+
+	Text string
 
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
 
-func (d *store) CreateUser(ctx context.Context, user User) error {
-	_, err := d.sql.ExecContext(ctx, `INSERT INTO users (email, username, password, uuid) VALUES (?, ?, ?, ?)`, user.Email, user.Username, user.Password, user.UUID)
+func (d *store) CreateCourse(ctx context.Context, course Course) error {
+	_, err := d.sql.ExecContext(ctx, `INSERT INTO courses (uuid, author_uuid, title, description, thumbnail, text) VALUES (?, ?, ?, ?, ?, ?)`,
+		course.UUID,
+		course.AuthorUUID,
+		course.Title,
+		course.Description,
+		course.Thumbnail,
+		course.Text,
+	)
 	return err
 }
 
-func (d *store) GetUserByEmail(ctx context.Context, email string) (*User, error) {
-	var u User
+func (d *store) GetCourseByUUID(ctx context.Context, uuid string) (*Course, error) {
+	var u Course
 
-	if err := d.sql.GetContext(ctx, &u, `SELECT * FROM users WHERE email=?`, email); err != nil {
+	if err := d.sql.GetContext(ctx, &u, `SELECT * FROM courses WHERE uuid=?`, uuid); err != nil {
 		return nil, err
 	}
 
 	return &u, nil
 }
 
-func (d *store) GetUserByUUID(ctx context.Context, UUID string) (*User, error) {
-	var u User
-
-	if err := d.sql.GetContext(ctx, &u, `SELECT * FROM users WHERE uuid=?`, UUID); err != nil {
+func (d *store) GetPublicCourses(ctx context.Context) ([]Course, error) {
+	rows, err := d.sql.QueryContext(ctx, `SELECT * from courses`)
+	if err != nil {
 		return nil, err
 	}
 
-	return &u, nil
+	defer rows.Close()
+
+	var (
+		courses []Course
+		buffer  Course
+	)
+
+	for rows.Next() {
+		if err := rows.Scan(
+			&buffer.ID,
+			&buffer.UUID,
+			&buffer.AuthorUUID,
+			&buffer.Title,
+			&buffer.Description,
+			&buffer.Thumbnail,
+			&buffer.Text,
+			&buffer.CreatedAt,
+			&buffer.UpdatedAt,
+		); err != nil {
+			d.lg.WithError(err).Error("failed to read course from db")
+		}
+
+		courses = append(courses, buffer)
+	}
+
+	return courses, nil
+}
+
+func (d *store) GetEnrolledCoursesForUser(ctx context.Context, userUUID string) ([]Course, error) {
+	rows, err := d.sql.QueryContext(ctx, `SELECT uuid, author_uuid, title, description, thumbnail FROM courses INNER JOIN enrolls ON enrolls.course_uuid = uuid WHERE enrolls.user_uuid = ?`,
+		userUUID,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		courses []Course
+		buffer  Course
+	)
+
+	for rows.Next() {
+		if err := rows.Scan(
+			&buffer.ID,
+			&buffer.UUID,
+			&buffer.AuthorUUID,
+			&buffer.Title,
+			&buffer.Description,
+			&buffer.Thumbnail,
+			&buffer.CreatedAt,
+			&buffer.UpdatedAt,
+		); err != nil {
+			d.lg.WithError(err).Error("failed to read course from db")
+		}
+
+		courses = append(courses, buffer)
+	}
+
+	return courses, nil
+}
+
+func (d *store) Enroll(ctx context.Context, userUUID, courseUUID string) error {
+	_, err := d.sql.ExecContext(ctx, `INSERT INTO enrolls (user_uuid, course_uuid) VALUES (?, ?)`,
+		userUUID,
+		courseUUID,
+	)
+	return err
+}
+
+func (d *store) Unenroll(ctx context.Context, userUUID, courseUUID string) error {
+	_, err := d.sql.ExecContext(ctx, `DELETE FROM enrolls WHERE user_uuid=? and course_uuid=?`,
+		userUUID,
+		courseUUID,
+	)
+	return err
 }
--- services/course/internal/store/db.go
+++ services/course/internal/store/db.go
@@ -6,16 +6,24 @@ import (
 	"github.com/FoxFurry/scholarlabs/services/common/db"
 	"github.com/FoxFurry/scholarlabs/services/course/internal/config"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
 
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 type DataStore interface {
-	// User
 
-	CreateUser(ctx context.Context, user User) error
-	GetUserByEmail(ctx context.Context, email string) (*User, error)
-	GetUserByUUID(ctx context.Context, userUUID string) (*User, error)
+	// Course
+	CreateCourse(ctx context.Context, course Course) error
+
+	GetCourseByUUID(ctx context.Context, uuid string) (*Course, error)
+	GetPublicCourses(ctx context.Context) ([]Course, error)
+	GetEnrolledCoursesForUser(ctx context.Context, userUUID string) ([]Course, error)
+
+	// Enrolls
+
+	Enroll(ctx context.Context, userUUID, courseUUID string) error
+	Unenroll(ctx context.Context, userUUID, courseUUID string) error
 
 	// DB
 
@@ -24,11 +32,12 @@ type DataStore interface {
 
 type store struct {
 	sql *sqlx.DB
+	lg  *logrus.Logger
 }
 
 func (d *store) GetDB() *sqlx.DB { return d.sql }
 
-func NewDataStore(cfg config.Config) (DataStore, error) {
+func NewDataStore(cfg config.Config, logger *logrus.Logger) (DataStore, error) {
 	database, err := db.NewDB(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPass, cfg.DBName)
 	if err != nil {
 		return nil, err
@@ -36,5 +45,6 @@ func NewDataStore(cfg config.Config) (DataStore, error) {
 
 	return &store{
 		sql: database,
+		lg:  logger,
 	}, nil
 }
