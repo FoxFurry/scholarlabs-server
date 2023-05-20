c2f0dbda24ad6825ce5bc8004dd72a5496a710f3
FoxFurry <isacartur@gmail.com>
2023-05-21 00:57:21 +0300

    add prototypes handlers to gateway

--- services/gateway/internal/server/prototypes.go
+++ services/gateway/internal/server/prototypes.go
@@ -1,13 +1,29 @@
 package server
 
 import (
+	"github.com/FoxFurry/scholarlabs/services/environment/proto"
+	"github.com/FoxFurry/scholarlabs/services/gateway/internal/models"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *ScholarLabs) GetPublicPrototypes(ctx *gin.Context) {
-	prototypes, err := s.environmentService.GetPublicPrototypes(ctx, &emptypb.Empty{})
+	prototypesResponse, err := s.environmentService.GetPublicPrototypes(ctx, &emptypb.Empty{})
 	if err != nil {
 		return
 	}
+
+	ctx.JSON(200, protoToModelPrototypes(prototypesResponse.Prototypes))
+}
+
+func protoToModelPrototypes(prototypes []*proto.PrototypeShort) []models.Prototype {
+	var modelsPrototypes []models.Prototype
+	for _, prototype := range prototypes {
+		modelsPrototypes = append(modelsPrototypes, models.Prototype{
+			UUID:             prototype.UUID,
+			Name:             prototype.Name,
+			ShortDescription: prototype.ShortDescription,
+		})
+	}
+	return modelsPrototypes
 }
