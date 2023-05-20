5edcf3fe3d84a1821789ee4fc8f611310ffa4316
FoxFurry <isacartur@gmail.com>
2023-05-21 00:55:33 +0300

    added placeholders for environment rooms

---
+++ services/environment/internal/service/room.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s *service) CreateRoom(ctx context.Context, environmentUUID string) (string, error) {
+	return "", fmt.Errorf("not implemented")
+}
+
+func (s *service) ResolveRoom(ctx context.Context, room string) (string, error) {
+	return "", fmt.Errorf("not implemented")
+}
